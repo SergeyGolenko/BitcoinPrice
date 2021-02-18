139813c8a236b003822feaea7b8d4f968f24ecbe
SergeyGolenko <1989sergiotay@gmail.com>
2021-02-18 11:54:42 +0200

    plus currency array

--- BitcoinPrice/ViewController.swift
+++ BitcoinPrice/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: Property
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    var finalURL = ""
+    
     
     //MARK: - IBOutlets
     @IBOutlet weak var pickerCurrency: UIPickerView!
@@ -19,9 +25,74 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //TODO: Place your 3 UIPickerView delegate methods here
     }
+    
+    
+    //
+    //    //MARK: - Networking
+    //    /***************************************************************/
+    //
+    //    func getWeatherData(url: String, parameters: [String : String]) {
+    //
+    //        Alamofire.request(url, method: .get, parameters: parameters)
+    //            .responseJSON { response in
+    //                if response.result.isSuccess {
+    //
+    //                    print("Sucess! Got the weather data")
+    //                    let weatherJSON : JSON = JSON(response.result.value!)
+    //
+    //                    self.updateWeatherData(json: weatherJSON)
+    //
+    //                } else {
+    //                    print("Error: \(String(describing: response.result.error))")
+    //                    self.bitcoinPriceLabel.text = "Connection Issues"
+    //                }
+    //            }
+    //
+    //    }
+    //
+    //
+    //
+    //
+    //
+    //    //MARK: - JSON Parsing
+    //    /***************************************************************/
+    //
+    //    func updateWeatherData(json : JSON) {
+    //
+    //        if let tempResult = json["main"]["temp"].double {
+    //
+    //        weatherData.temperature = Int(round(tempResult!) - 273.15)
+    //        weatherData.city = json["name"].stringValue
+    //        weatherData.condition = json["weather"][0]["id"].intValue
+    //        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
+    //        }
+    //
+    //        updateUIWithWeatherData()
+    //    }
+    //
 
 
+
+
+}
+
+
+
+extension ViewController : UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
+    
+    
+}
+
+extension ViewController : UIPickerViewDelegate {
+    
 }
 

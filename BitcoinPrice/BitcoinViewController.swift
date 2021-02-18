ee62d8514ff00498f2854c568320afbc57e5a46f
SergeyGolenko <1989sergiotay@gmail.com>
2021-02-18 16:01:22 +0200

    done

--- BitcoinPrice/BitcoinViewController.swift
+++ BitcoinPrice/BitcoinViewController.swift
@@ -6,22 +6,28 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+
 
 class BitcoinViewController: UIViewController {
     
     //MARK: Property
-    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    var baseURL : String  { "https://api.nomics.com/v1/currencies/ticker?key=9efd5f974b22088e1fd0b7e27be022e1&ids=BTC,ETH,XRP&interval=1d,30d&convert=\(self.convertValut)&per-page=100&page=1"
+    }
+    
+    let KEY_Bitcoin = "9efd5f974b22088e1fd0b7e27be022e1"
+    
+    var convertValut = "EUR"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
-    var finalURL = ""
+    
+    
     
     
     //MARK: - IBOutlets
     @IBOutlet weak var pickerCurrency: UIPickerView!
     @IBOutlet weak var priceLabel: UILabel!
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,49 +35,33 @@ class BitcoinViewController: UIViewController {
         pickerCurrency.delegate = self
         pickerCurrency.dataSource = self
     }
-    
-    
-    //
     //    //MARK: - Networking
     //    /***************************************************************/
-    //
-    //    func getWeatherData(url: String, parameters: [String : String]) {
-    //
-    //        Alamofire.request(url, method: .get, parameters: parameters)
-    //            .responseJSON { response in
-    //                if response.result.isSuccess {
-    //
-    //                    print("Sucess! Got the weather data")
-    //                    let weatherJSON : JSON = JSON(response.result.value!)
-    //
-    //                    self.updateWeatherData(json: weatherJSON)
-    //
-    //                } else {
-    //                    print("Error: \(String(describing: response.result.error))")
-    //                    self.bitcoinPriceLabel.text = "Connection Issues"
-    //                }
-    //            }
-    //
-    //    }
+    
+        func getBitcoinPrice(url: String) {
+            AF.request(url,method: .get)
+                .responseJSON { response in
+                    if response.error != nil {
+                        print("Error")
+                        self.priceLabel.text = "Connection Issues"
+                    } else {
+                        //
+                        print("Sucess! Got the weather data")
+                        let bitcoinJSON : JSON = JSON(response.value!)
+                        self.updateBitcoinData(json: bitcoinJSON)
+                    }
+                }
+    
+        }
     
     //    //MARK: - JSON Parsing
     //    /***************************************************************/
-    //
-    //    func updateWeatherData(json : JSON) {
-    //
-    //        if let tempResult = json["main"]["temp"].double {
-    //
-    //        weatherData.temperature = Int(round(tempResult!) - 273.15)
-    //        weatherData.city = json["name"].stringValue
-    //        weatherData.condition = json["weather"][0]["id"].intValue
-    //        weatherData.weatherIconName =    weatherData.updateWeatherIcon(condition: weatherData.condition)
-    //        }
-    //
-    //        updateUIWithWeatherData()
-    //    }
-    //
+        func updateBitcoinData(json : JSON) {
+            let result = json[0]["price"].string
+            priceLabel.text = result
+        }
+    
 }
-
 
 
 extension BitcoinViewController : UIPickerViewDataSource {
@@ -79,25 +69,20 @@ extension BitcoinViewController : UIPickerViewDataSource {
         return 1
     }
     
-    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return currencyArray.count
     }
-    
-  
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return currencyArray[row]
     }
     
-    
 }
 
 extension BitcoinViewController : UIPickerViewDelegate {
-    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        finalURL = baseURL + currencyArray[row]
-        print(finalURL)
+        convertValut = currencyArray[row]
+        getBitcoinPrice(url: baseURL)
     }
 }
 
